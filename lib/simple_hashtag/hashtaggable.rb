90ae51402c3ad9ae126188ae5120ca4caab92370
Alexander Polovnikov <aleksandrpol@me.com>
2015-09-08 17:51:06 +0300

    Update hashtaggable.rb

--- lib/simple_hashtag/hashtaggable.rb
+++ lib/simple_hashtag/hashtaggable.rb
@@ -29,6 +29,7 @@ module SimpleHashtag
 
       def scan_for_hashtags(content)
         match = content.scan(Hashtag::HASHTAG_REGEX)
+        match.downcase!
         match.uniq!
         match
       end
