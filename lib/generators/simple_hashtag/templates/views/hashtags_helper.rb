57db103d473a00344e487e83cbe6694d97aa3753
Alexander Polovnikov <aleksandrpol@me.com>
2015-09-16 03:01:05 +0300

    Update hashtags_helper.rb

--- lib/generators/simple_hashtag/templates/views/hashtags_helper.rb
+++ lib/generators/simple_hashtag/templates/views/hashtags_helper.rb
@@ -1,14 +1,14 @@
 module HashtagsHelper
   def linkify_hashtags(hashtaggable_content)
     regex = SimpleHashtag::Hashtag::HASHTAG_REGEX
-    hashtagged_content = hashtaggable_content.to_st_r.gsub(regex) do
+    hashtagged_content = hashtaggable_content.to_s.gsub(regex) do
       link_to($&, hashtag_path($2), {class: :hashtag})
     end
     hashtagged_content.html_safe
   end
 
   def render_hashtaggable(hashtaggable)
-    klass        = hashtaggable.class.to_st_r.underscore
+    klass        = hashtaggable.class.to_s.underscore
     view_dirname = klass.pluralize
     partial      = klass
     render "#{view_dirname}/#{partial}", {klass.to_sym => hashtaggable}
